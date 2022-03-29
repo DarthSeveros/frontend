8c7aa4a28067da390f6d6899dc7533ad2b5c2496
José Velásquez <joseivecas@gmail.com>
2022-03-29 12:09:56 -0400

    Update funciones_peso_ideal.js

--- js/funciones_peso_ideal.js
+++ js/funciones_peso_ideal.js
@@ -7,7 +7,7 @@ function verificateAge(age){
 
 function viricateWeight(weight) {
     //Chequeamos peso fuera de 1 a 100
-    if (weight < 1 || wieght > 100) {
+    if (weight < 1 || weight > 100) {
         return true;
     }
 }
